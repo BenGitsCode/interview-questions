de3c6e4a76ffbb85130c84d4190d1bca6fa53216
Ben Adamski <benjadamski@gmail.com>
2017-05-23 13:26:19 -0400

    Add challenge section and ruby reverse
    -  I've gotten this challenge too many times to not save it somewhere for
    reference.
    -  Adds challenge dir and ruby dir within it.

---
+++ challenges/ruby/reverse_without_reverse.rb
@@ -0,0 +1,24 @@
+string = "This is an example"
+
+def reverse(string)
+  tmp_arr = []
+  reversed_arr = []
+  
+  i = string.length
+  
+  tmp_array.push(string.split(""))
+  
+  tmp_array.each do |item|
+    
+    until i < 0
+  reverse_array.push(item[i])
+  i -= 1
+end
+
+# print reverse_array.inspect
+
+end
+
+reverse_array.join
+
+end
