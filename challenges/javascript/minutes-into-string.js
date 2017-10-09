5819ee0cccd8fb1b1d338156a6e8ffd7955b851d
Ben Adamski <benjadamski@gmail.com>
2017-10-09 16:08:14 -0400

    Start with some pseudo code and outlining
    - Start an empty function (naming things is hard) and put in some pseudo code
    after looking at it for a bit I sense sort of an abstracted fizzbuzz pattern
    coming up with a slight twist. I know simplest gotcha pieces of fizzbuzz is
    starting with the most complex condition first, to catch all pieces of it.
    - I think I'm going to start with the days and output. No other conditions.
    Then same for hours, then minutes, then I'll focus on the zero stripping and
    the string interpolation. Then refactor.
    - Make it work, make it right, make it better.

--- challenges/javascript/minutes-into-string.js
+++ challenges/javascript/minutes-into-string.js
@@ -1,2 +1,16 @@
 // 1441 minutes into " 1 day and 1 minute "
 // strip away any zeros i.e. no "0 days 4 hours 1 min" or " 1 day 12 hours 0 minutes"
+
+let timeData = 1441;
+
+// divide into days
+// divide into hours
+// divide into minutes
+
+// strip away any and all zeros
+// interpolate integers into string
+
+function timeIntoString(timeData) {
+    let day = 0
+
+}
