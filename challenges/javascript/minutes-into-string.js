0f8e7f089fd35c7883e6bc8ea3c122f7406c9693
Ben Adamski <benjadamski@gmail.com>
2017-10-09 17:10:45 -0400

    FIX THIS COMMIT
    NEVER PUSH THIS SHITTY COMMIT

--- challenges/javascript/minutes-into-string.js
+++ challenges/javascript/minutes-into-string.js
@@ -1,7 +1,7 @@
 // 1441 minutes into " 1 day and 1 minute "
 // strip away any zeros i.e. no "0 days 4 hours 1 min" or " 1 day 12 hours 0 minutes"
 
-let timeData = 1441;
+let timeData = 1441
 
 // divide into days
 // divide into hours
@@ -10,7 +10,33 @@ let timeData = 1441;
 // strip away any and all zeros
 // interpolate integers into string
 
-function timeIntoString(timeData) {
-    let day = 0
+function timeIntoDays(timeData) {
+  let days = timeData / 60
+  console.log(days)
 
+  if (days >= 24 && days < 48) {
+    let days = 1
+    console.log('1 or more days ', days)
+  } else {
+    let days = 0
+    console.log('less than 1 day', days)
+  }
+  return days
+  // unreachable code with current example
+  // console.log(days)
+}
+// timeIntoDays(timeData)
+
+function timeIntoHours(timeData) {
+  let hrs = timeData / 60
+
+  console.log(hrs)
+  console.log(hrsTest)
+  //   if (condition) {
+  //   }
+}
+timeIntoHours(timeData)
+
+function timeLeft(timeData) {
+  //
 }
