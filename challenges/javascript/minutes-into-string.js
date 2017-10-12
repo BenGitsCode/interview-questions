1484ad3cb98de785c35e765f044ab82e987aa313
Ben Adamski <benjadamski@gmail.com>
2017-10-12 11:12:44 -0400

    Refactor minutes to string to handle edge cases.
    Move this and all solutions code to a separate solution branch.

--- challenges/javascript/minutes-into-string.js
+++ challenges/javascript/minutes-into-string.js
@@ -2,26 +2,30 @@
 // strip away any zeros i.e. no "0 days 4 hours 1 min" or " 1 day 12 hours 0 minutes"
 
 let findTime = (t) => {
-  // 5020 test case
-  const times = ['day', 'hour', 'minute']
-  let totalHours = Math.floor(t / 60)
-  //totalHours = 83
-  let minRemain = t % 60 === 0 ? t : t % 60 // log at end
-  // minRemain = 40
-  if (totalHours > 24) {
-    let totalDays = Math.floor(totalHours / 24) // log at end
-    // totalDays = 3
-    let hoursRemain = totalHours - totalDays * 24
-    // hoursRemain = 11
-    console.log(`${totalDays} ${times[0]}${totalDays > 1 ? 's' : ''} ${hoursRemain} ${times[1]}${hoursRemain > 1 ? 's' : ''} ${minRemain} ${times[2]} ${minRemain > 1 ? 's' : ''}`)
-  } else {
-    console.log(`${totalHours} ${times[1]} ${totalHours > 1 ? 's' : ''}${times[2]}${minRemain > 1 ? 's' : ''}`)
+  let display = [];
+  let totalHours = Math.floor(t / 60);
+  let totalDays = Math.floor(totalHours / 24);
+  let hoursRemain = totalHours - totalDays * 24;
+  let minRemain = t % 60 === 0 ? t : t % 60; // log at end
+  if (totalHours >= 24) {
+    let label = totalDays > 1 ? 'Days' : "Day"
+    display.push(totalDays, label)
   }
-}
+  if (hoursRemain >= 1) {
+    let label = totalHours > 1 ? 'Hours' : "Hour"
+    display.push(hoursRemain, label)
+  }
+  if (minRemain >= 1) {
+    let label = minRemain > 1 ? 'Minutes' : "Minute"
+    display.push(minRemain, label)
+  }
+  display = display.join(' ')
+  console.log(display)
+};
 
 //works 👍
 // findTime(5020)
 
-// interpolation issue
+// interpolation issues
 findTime(1441)
 findTime(3299)
