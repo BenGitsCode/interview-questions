9a882474e44fe5a70a1f1f3eedfbb19804f1002e
Ben Adamski <benjadamski@gmail.com>
2017-10-12 11:16:43 -0400

    Remove all solutions from js challenges
    - All currently on solutions branch.

--- challenges/javascript/wayfair.js
+++ challenges/javascript/wayfair.js
@@ -4,60 +4,6 @@ example input: [-6, 2, 1, 2, -10, -4, 8]
 example output: [-6, -10, -4, 2, 1, 2, 8]
 **/
 
-// iteration
-
-// less than or greater than 0 if /else check
-
-// push into new array
-
-// do negatives first to keep with negs on left pos on right requirement
-
-//Decarling arrays needed
-var array = [-6, 2, 1, 2, -10, -4, 8];
-var newArr = [];
-var posArr = [];
-var negArr = [];
-
-
-for (var i = 0; i < array.length; i++) {
-    //console.log(array[i]);
-    if (array[i] < 0) {
-     newArr.push(array[i]);
-    }
-    console.log(newArr);
-}
-
-for (var i = 0; i < array.length; i++) {
-  //console.log(array[i]);
-  if (array[i] > 0) {
-   newArr.push(array[i]);
-  }
-
-  console.log(newArr);
-}
-
-
-/* Refactored to use only one for loop
-
-for (var i = 0; i < array.length; i++) {
- if (array[i] < 0) {
-  posArr.push(array[i]);
- } else {
-  negArr.push(array[i]);
- }
- newArr = posArr.concat(negArr);
- console.log(newArr);
-}
-
-*/
-
-
-
-
-
-
-
-
 /*============talk through problem============
 
 Desccribe how you would go about creating a function to select k random items out of an input array of size n. The input array contains only unique items, and the result set should be only unique items.
