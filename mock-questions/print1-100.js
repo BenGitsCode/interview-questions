871dc1b6da1940d2a9c2eeaacc0a27072a6d993a
Ben Adamski <benjadamski@gmail.com>
2017-10-20 12:07:20 -0400

    Add much simpler solution
    - I feel insane for never thinking of this -_-

--- mock-questions/print1-100.js
+++ mock-questions/print1-100.js
@@ -2,19 +2,29 @@
 
 let i
 
- for (i=100; i > 0; i--)
- {
-      // produce the code that will print out 1 to 100 inclusively.
-      // console.log(101-i)
-
- }
-
-let i 
-
-for (i=100; i > 0; i--) {
-//produce the code that will print out 1 to 100 inclusively.
-
-console.log(101-i)
+for (i = 100; i > 0; i--) {
+  // produce the code that will print out 1 to 100 inclusively.
+  // console.log(101-i)
 }
 
+let i
 
+for (i = 100; i > 0; i--) {
+  //produce the code that will print out 1 to 100 inclusively.
+
+  console.log(101 - i)
+}
+
+const hundo = function() {
+  for (let i = 0; i < 101; i++) {
+    console.log(i)
+  }
+}
+
+const hundo = function() {
+  for (let i = 100; i > 0; i--) {
+    console.log(101 - i)
+  }
+}
+
+hundo()
